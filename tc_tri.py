2bd562c8999d1d028e3ced505475156ad3b1dfa0
Cole-Narfason <48136863+Cole-Narfason@users.noreply.github.com>
2022-09-16 22:32:14 -0600

    Update tc_tri.py

--- tc_tri.py
+++ tc_tri.py
@@ -354,44 +354,5 @@ if qtz <= 5:
 col1.success("The rock can be classified as: {}".format(result))
 
 #result = ' '
-#-------------------------------------------------------
-#st.success(result)
-
-
-
-# def load_data(uploaded_file):
-    # if uploaded_file is not None:
-        # try:
-            # bytes_data = uploaded_file.read()
-            # str_io = StringIO(bytes_data.decode('Windows-1252'))
-            # las_file = lasio.read(str_io)
-            # well_data = las_file.df()
-            # well_data['DEPTH'] = well_data.index
-
-        # except UnicodeDecodeError as e:
-            # st.error(f"error loading log.las: {e}")
-    # else:
-        # las_file = None
-        # well_data = None
-
-    # return las_file, well_data
-
-
-# #TODO
-# def missing_data():
-    # st.title('Missing Data')
-    # missing_data = well_data.copy()
-    # missing = px.area(well_data, x='DEPTH', y='DT')
-    # st.plotly_chart(missing)
-
-# # Sidebar Options & File Uplaod
-# las_file=None
-# st.sidebar.write('# LAS Data Explorer')
-# st.sidebar.write('To begin using the app, load your LAS file using the file upload option below.')
-
-# uploadedfile = st.sidebar.file_uploader(' ', type=['.las'])
-# las_file, well_data = load_data(uploadedfile)
-
-# if las_file:
-    # st.sidebar.success('File Uploaded Successfully')
-    # st.sidebar.write(f'<b>Well Name</b>: {las_file.well.WELL.value}',unsafe_allow_html=True)
+except NameError as e:
+    print('please input data')
