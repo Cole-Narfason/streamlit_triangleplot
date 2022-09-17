d9abe59cf8dbc7c84644fcf3fed953b4bfbe4d93
Cole-Narfason <48136863+Cole-Narfason@users.noreply.github.com>
2022-09-16 22:26:25 -0600

    Update tc_tri.py

--- tc_tri.py
+++ tc_tri.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from PIL import Image
-#import plotly.graph_objects as go
+import plotly.graph_objects as go
 import plotly.express as px
 import plotly.io as pio
 
